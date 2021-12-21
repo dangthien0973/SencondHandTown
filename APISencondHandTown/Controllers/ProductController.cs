239acf43f88a056de8847492d4ac6206ae49f4d4
Đăng Thiện <71754331+dangthien0973@users.noreply.github.com>
2021-12-21 15:48:16 +0700

    Revert "add product"

--- APISencondHandTown/Controllers/ProductController.cs
+++ APISencondHandTown/Controllers/ProductController.cs
@@ -12,13 +12,11 @@ namespace APISencondHandTown.Controllers
     {
         private readonly IRepository<Product> _productRepository;
         private readonly IProductRepository _ProductRepositoryMoreMethod;
-        private readonly IRepository<ProductDetail> _productDetailRepository;
        
-        public ProductController(IProductRepository _ProductRepositoryMoreMethod, IRepository<Product> _productRepository, IRepository<ProductDetail> _productDetailRepository)
+        public ProductController(IProductRepository _ProductRepositoryMoreMethod, IRepository<Product> _productRepository)
         {
             this._productRepository = _productRepository;
-            this._ProductRepositoryMoreMethod = _ProductRepositoryMoreMethod;
-            this._productDetailRepository = _productDetailRepository;
+           this._ProductRepositoryMoreMethod = _ProductRepositoryMoreMethod;
         }
         [HttpPost("getProducList")]
         public IActionResult getProductList(ProductPageDto productPage)
@@ -39,47 +37,5 @@ namespace APISencondHandTown.Controllers
                     );
         }
        
-       [HttpPost("addProduct")]
-        public IActionResult addProduct(ProdductModelDto productDto)
-        {
-            try
-            {
-                var product = new Product()
-                {
-                    Price = productDto.Price1,
-                    PriceSale = productDto.PriceSale1,
-                    Amount = productDto.Amount1,
-
-                };
-                
-                var productDetail = new ProductDetail()
-                {
-
-                    NameProduct = productDto.NameProduct1,
-                    SourceOrigin = productDto.SourceOrigin1,
-                    Descriptions = productDto.Descriptions1,
-
-                };
-                
-
-                _productDetailRepository.Add(productDetail);
-                _productDetailRepository.SaveChanges();
-                _productRepository.Add(product);
-                _productRepository.SaveChanges();
-
-                return Ok(new
-                {
-                    Message = "success addProduct",
-                    product,
-                    productDetail
-                });
-            }
-            catch (Exception e)
-            {
-                return Ok(e.Message);
-            }
-
-        }
-
     }
 }
